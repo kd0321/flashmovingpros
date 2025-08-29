72d3505ab0c7e4fc80b09742892067c86025c438
kd0321 <dabeaukhalid@gmail.com>
2025-08-29 01:29:51 +0000

    Update service pages and scripts

--- COMPONENTS/script.js
+++ COMPONENTS/script.js
@@ -11,6 +11,26 @@ document.addEventListener('DOMContentLoaded', () => {
     'residential-moving' : 'service_pages/residential.html', 
     'appliance-moving' : 'service_pages/appliance.html', 
     'specialty-items' : 'service_pages/speciality.html', 
+    'junk-removal' : 'service_pages/junk.html',
+    'furniture-delivery' : 'service_pages/furniture_delivery.html',
+    'event-equipment-delivery' : 'service_pages/event_equipment.html',
+    'furniture-assembly' : 'service_pages/furniture_assembly.html',
+    'local-delivery' : 'service_pages/local_delivery.html',
+    'hauling' : 'service_pages/hauling.html',
+    'storage' : 'service_pages/storage.html',
+    'attic-cleanout' : 'service_pages/attic_cleanout.html',
+    'basement-cleanout' : 'service_pages/basement_cleanout.html',
+    'garage-cleanout' : 'service_pages/garage_cleanout.html',
+    'storage-unit-cleanout' : 'service_pages/storage_unit_cleanout.html',
+    'hoarder-cleanout' : 'service_pages/hoarder_cleanout.html',
+    'estate-cleanout' : 'service_pages/estate_cleanout.html',
+    'office-cleanout' : 'service_pages/office_cleanout.html',
+    'shed-deck-cleanout' : 'service_pages/shed_deck_cleanout.html',
+    'renovation-debris' : 'service_pages/renovation_debris.html',
+    'post-construction' : 'service_pages/post_construction.html',
+    'foreclosure-cleanout' : 'service_pages/foreclosure_cleanout.html',
+    'eviction-cleanout' : 'service_pages/eviction_cleanout.html',
+
 
 
     };
